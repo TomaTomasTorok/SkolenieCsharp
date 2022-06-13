1bcb812a401d18eb1698724a04e2254e89159405
tomáš Torok <tomas.torok66@gmail.com>
2022-06-13 13:00:43 +0200

    FAResult

--- CNET2/ConsoleApp1/Program.cs
+++ CNET2/ConsoleApp1/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Model;
+
 Console.WriteLine("Hello, World!");
+FAResult fAResult  = new FAResult("file",SourceType.FILE);
+
+
+Console.WriteLine(fAResult);
+
+Console.WriteLine(fAResult);
---
+++ CNET2/Model/FAResult.cs
@@ -0,0 +1,36 @@
+﻿namespace Model
+{
+    /// <summary>
+    /// Výsledok frekvenčnej analýzy pre jeden zdroj (súbor alebo URL)
+    /// </summary>
+    public class FAResult
+    {
+ public FAResult(string source, SourceType sourceType)
+        {
+            Source = source;
+            SourceType = sourceType;
+           
+            ddddd
+        }
+        public FAResult() { }
+
+        /// <summary>
+        /// zdroj textu
+        /// </summary>
+        public string Source { get; set; }
+        public SourceType  SourceType { get; set; } 
+        /// <summary>
+        /// Vysledna frekvenc. anal
+        /// </summary>
+        public  Dictionary<string, int> Words { get; set; }=new Dictionary<string, int>();
+        public override string ToString()
+        {
+            return $"{Source} {Words?.Count}";
+        }
+    }
+    public enum SourceType 
+    {
+        URL,
+        FILE
+    }
+}

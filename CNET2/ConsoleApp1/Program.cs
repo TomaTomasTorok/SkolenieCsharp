5a0fd5612b57360902fdc4a6f458e5f3fdbf67c6
tomáš Torok <tomas.torok66@gmail.com>
2022-06-13 16:28:40 +0200

    vsetko v jednom

--- CNET2/ConsoleApp1/Program.cs
+++ CNET2/ConsoleApp1/Program.cs
@@ -1,11 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using System.Linq;
+using Data;
 using Model;
 
 Console.WriteLine("Hello, World!");
-FAResult fAResult  = new FAResult("file",SourceType.FILE);
+//FAResult fAResult  = new FAResult("file",SourceType.FILE);
+//var x = await FreqAnalysis.FreqAnalysisForURL("https://raw.githubusercontent.com/kubicek-skoleni/NET6_CNET2/master/Books/alice.txt");
+//Console.WriteLine( x);
 
 
-Console.WriteLine(fAResult);
+//var booksdir = @"C:\Users\StudentEN\source\repos\TomasT";
+//var files = Directory.EnumerateFiles(booksdir, "*.txt");
+//// var result = await FreqAnalysis.FreqAnalysisForURL("https://raw.githubusercontent.com/kubicek-skoleni/NET6_CNET2/master/Books/alice.txt");
+//foreach (var file in files)
+//{
+//    var result = FreqAnalysis.FreqAnalysisForFile(file);
+//    var ordered = result.OrderByDescending(x => x.Value).Take(10);
+//    foreach (var item in ordered)
+//    {
+//        Console.WriteLine($"{item.Key}: {item.Value}");
+//    }
 
-Console.WriteLine(fAResult);
+//    Console.WriteLine(ordered);
+//}
+
+var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+var numbers1 = numbers.Select(n=> n.GetTypeCode());
+
+Console.WriteLine(numbers1);
+
+
+//foreach (int n in numbers1)
+//{
+//    Console.Writeline(n);
+//}
--- CNET2/Data/FreqAnalysis.cs
+++ CNET2/Data/FreqAnalysis.cs
@@ -1,25 +1,55 @@
 ﻿namespace Data
+
 {
     public class FreqAnalysis
     {
         public static Dictionary<string, int> FreqAnalysisFromString(string input) 
-        {
-            throw new NotFiniteNumberException();
+        { 
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            input.Replace(".", " ");
+            input.Replace(",", string.Empty);
+            input.Replace(":", " ");
+            string[] words = input.Split(' ',StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries );
+            
+
+            List<string> listWithoutDuplicates = words.Distinct().ToList();
+            var q = from x in words
+                    group x by x into g
+                    let count = g.Count()
+                    orderby count descending
+                    select new {Value = g.Key, Count = count};
+            foreach (var x in q) {
+               
+                dict.Add(key:x.Value, value: x.Count);
+
+              
+            }
+
+
+            return dict;
         }
 
-        public static async Task<Dictionary<string, int>> FreqAnalysisForURL(string url) 
+        public static async Task<Dictionary<string, int>> FreqAnalysisForURL(string url)
         {
-       HttpClient httpClient = new HttpClient();
-            var content = await httpClient.GetStringAsync(url);
-            return FreqAnalysisFromString(content);
+            HttpClient httpClient = new HttpClient();
+            var content1 = await httpClient.GetStringAsync(url);
+           
+            return FreqAnalysisFromString(content1);
         }
 
-        public static Dictionary<string, int> FreqAnalysisForFile(string file) 
+        // public static async Task<List<string>> FreqAnalysisForURL(string url) 
+        // {
+        //HttpClient httpClient = new HttpClient();
+        //     var content = await httpClient.GetStringAsync(url);
+        //     return FreqAnalysisFromString(content);
+        // }
+
+        public static Dictionary<string, int> FreqAnalysisForFile(string file)
         {
             var content = File.ReadAllText(file);
-        return     FreqAnalysisFromString(content);
+            return FreqAnalysisFromString(content);
 
-      
+
         }
 
 

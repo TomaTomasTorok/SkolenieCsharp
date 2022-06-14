0a3b5f413680f1a81dc4b92c5cb82aacd8594f9f
tomáš Torok <tomas.torok66@gmail.com>
2022-06-14 10:47:52 +0200

    wpf

--- CNET2/ConsoleApp1/Program.cs
+++ CNET2/ConsoleApp1/Program.cs
@@ -4,31 +4,31 @@ using Data;
 using Model;
 
 Console.WriteLine("Hello, World!");
-//FAResult fAResult  = new FAResult("file",SourceType.FILE);
-//var x = await FreqAnalysis.FreqAnalysisForURL("https://raw.githubusercontent.com/kubicek-skoleni/NET6_CNET2/master/Books/alice.txt");
-//Console.WriteLine( x);
+FAResult fAResult = new FAResult("file", SourceType.FILE);
+var x = await FreqAnalysis.FreqAnalysisForURL("https://raw.githubusercontent.com/kubicek-skoleni/NET6_CNET2/master/Books/alice.txt");
+Console.WriteLine(x);
 
 
-//var booksdir = @"C:\Users\StudentEN\source\repos\TomasT";
-//var files = Directory.EnumerateFiles(booksdir, "*.txt");
-//// var result = await FreqAnalysis.FreqAnalysisForURL("https://raw.githubusercontent.com/kubicek-skoleni/NET6_CNET2/master/Books/alice.txt");
-//foreach (var file in files)
-//{
-//    var result = FreqAnalysis.FreqAnalysisForFile(file);
-//    var ordered = result.OrderByDescending(x => x.Value).Take(10);
-//    foreach (var item in ordered)
-//    {
-//        Console.WriteLine($"{item.Key}: {item.Value}");
-//    }
+var booksdir = @"C:\Users\StudentEN\source\repos\TomasT";
+var files = Directory.EnumerateFiles(booksdir, "*.txt");
+// var result = await FreqAnalysis.FreqAnalysisForURL("https://raw.githubusercontent.com/kubicek-skoleni/NET6_CNET2/master/Books/alice.txt");
+foreach (var file in files)
+{
+    var result = FreqAnalysis.FreqAnalysisForFile(file);
+    var ordered = result.OrderByDescending(x => x.Value).Take(10);
+    foreach (var item in ordered)
+    {
+        Console.WriteLine($"{item.Key}: {item.Value}");
+    }
 
-//    Console.WriteLine(ordered);
-//}
+    Console.WriteLine(ordered);
+}
 
-var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+//var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 
-var numbers1 = numbers.Select(n=> n.GetTypeCode());
+//var numbers1 = numbers.Select(n=> n.GetTypeCode());
 
-Console.WriteLine(numbers1);
+//Console.WriteLine(numbers1);
 
 
 //foreach (int n in numbers1)
